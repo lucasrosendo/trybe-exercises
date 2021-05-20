bbb98a00edb6ae1a1285a6059e7ff1d276a19d4b
Lucas Oliveira Sousa Dias Rosendo <lucasrosendo91@gmail.com>
2021-05-20 18:31:25 -0300

    Exercicios até o 7 do dia 4.1

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/dia-4.1/2-maior-de-dois-numeros.js
@@ -0,0 +1,8 @@
+let a= 5;
+let b= 20;
+
+if (a>b){
+ console.log(a);
+}else{
+  console.log(b);
+}
---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/dia-4.1/5-triangulo.js
@@ -0,0 +1,16 @@
+let a1=100;
+let a2=50;
+let a3= 30;
+
+if (a1 + a2 + a3 == 180){
+  console.log(true);
+}else if(a1 < 0){
+  console.log("erro");
+}else if(a2 < 0){
+  console.log("erro");
+}else if(a3 < 0){
+  console.log("erro");
+}
+else{
+  console.log(false);
+} 
